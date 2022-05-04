6ec7f95ca7c71f78e3f037610f6a504bc3347ed0
Aminejv <elouartinra@hotmail.com>
2022-05-04 17:05:06 +0000

    feat(hooks): add useOnWindowBlur

--- src/Common/hooks.js
+++ src/Common/hooks.js
@@ -19,3 +19,27 @@ export const useEventListener = (
 
 export const useIsomorphicLayoutEffect =
   typeof window !== "undefined" ? React.useLayoutEffect : React.useEffect;
+
+export const useOnWindowBlur = (callback) => {
+  React.useEffect(() => {
+    var hidden, visibilityChange;
+    if (typeof document.hidden !== "undefined") {
+      // Opera 12.10 and Firefox 18 and later support
+      hidden = "hidden";
+      visibilityChange = "visibilitychange";
+    } else if (typeof document.msHidden !== "undefined") {
+      hidden = "msHidden";
+      visibilityChange = "msvisibilitychange";
+    } else if (typeof document.webkitHidden !== "undefined") {
+      hidden = "webkitHidden";
+      visibilityChange = "webkitvisibilitychange";
+    }
+
+    const handleVisibilityChange = () => {
+      if (document[hidden]) callback();
+    };
+    window.addEventListener(visibilityChange, handleVisibilityChange, false);
+    return () =>
+      window.removeEventListener(visibilityChange, handleVisibilityChange);
+  }, []);
+};
