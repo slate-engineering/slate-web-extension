f39e72f3bcb16139ef4d1da9f8b8935d6be07ef4
Aminejv <elouartinra@hotmail.com>
2022-05-18 15:09:08 +0100

    feat(Switch): add Switch component

---
+++ src/Components/Switch.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+export const Match = React.memo((/** { component, when } */) => {
+  return null;
+});
+
+// NOTE(amine): displayName is used to assert that direct children of Switch are the Match components
+Match.displayName = "$";
+
+export const Switch = React.forwardRef(({ children, fallback = null }, ref) => {
+  if (Array.isArray(children)) {
+    for (let element of children) {
+      if (element.type.displayName !== "$")
+        console.error(
+          "Switch component requires Match component as its children"
+        );
+
+      if (element.props.when) {
+        const { component: Component, ...props } = element.props;
+        return <Component ref={ref} {...props} />;
+      }
+    }
+
+    return fallback;
+  }
+
+  if (children?.props?.when) return children;
+
+  return fallback;
+});
