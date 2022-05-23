54f419e69c3d23b32bf0c06b01f4a35e0a5f8fc5
Aminejv <elouartinra@hotmail.com>
2022-05-23 18:53:12 +0100

    feat(Navigation): unmout the app before removing it from the dom

--- src/App.js
+++ src/App.js
@@ -4,65 +4,20 @@ import * as Navigation from "./Core/navigation/app";
 import HistoryScene from "./scenes/history";
 import ShadowDom from "./Components/ShadowDom";
 
+import { NavigationProvider } from "./Core/navigation/app/jumper";
 import { useOnWindowBlur } from "./Common/hooks";
 
-// const useOg = () => {
-//   const [og, setOg] = React.useState({ image: null, title: null });
-//   React.useEffect(() => {
-//     const getMeta = () => {
-//       let meta = {};
-
-//       if (document.querySelector("meta[property='og:image']")) {
-//         meta.image = document
-//           .querySelector("meta[property='og:image']")
-//           .getAttribute("content");
-//       }
-
-//       if (document.querySelector("link[rel~='icon']")) {
-//         meta.favicon = document
-//           .querySelector("link[rel~='icon']")
-//           .getAttribute("href");
-//       }
-
-//       return meta;
-//     };
-
-//     let meta = getMeta();
-//     setOg({ image: meta.image, favicon: meta.favicon });
-//   }, []);
-//   return og;
-// };
-
 function App() {
-  // const [checkLink, setCheckLink] = React.useState({
-  //   uploaded: false,
-  //   data: null,
-  // });
-  // React.useEffect(() => {
-  //   let handleMessage = (event) => {
-  //     if (
-  //       event.data.type === "CHECK_LINK" &&
-  //       event.data.data.decorator === "LINK_FOUND"
-  //     ) {
-  //       setCheckLink({ uploaded: true, data: event.data.data });
-  //     }
-  //   };
-
-  //   window.addEventListener("message", handleMessage);
-
-  //   return () => window.removeEventListener("message", handleMessage);
-  // }, []);
-
-  // const og = useOg();
-
   useOnWindowBlur(Navigation.closeExtensionJumper);
 
   return (
-    <div style={{ all: "initial" }}>
-      <ShadowDom>
-        <HistoryScene />
-      </ShadowDom>
-    </div>
+    <NavigationProvider>
+      <div style={{ all: "initial" }}>
+        <ShadowDom>
+          <HistoryScene />
+        </ShadowDom>
+      </div>
+    </NavigationProvider>
   );
 }
 
--- src/Core/navigation/app/jumper.js
+++ src/Core/navigation/app/jumper.js
@@ -1,8 +1,36 @@
+import * as React from "react";
+
 import { messages } from "../";
 
 export const openUrls = ({ urls, query }) =>
   window.postMessage({ type: messages.openURLsRequest, urls, query }, "*");
 
-export const closeExtensionJumper = () => {
+const closeExtensionJumper = () => {
   window.postMessage({ type: messages.closeExtensionJumperRequest }, "*");
+};
+
+const NavigationContext = React.createContext({ open: true });
+
+export const useNavigation = () => React.useContext(NavigationContext);
+
+export const NavigationProvider = ({ children }) => {
+  const [isOpen, setIsOpen] = React.useState(true);
+
+  const value = React.useMemo(
+    () => ({ isOpen: isOpen, closeTheJumper: () => setIsOpen(false) }),
+    [isOpen]
+  );
+
+  React.useEffect(() => {
+    if (!isOpen) closeExtensionJumper();
+  }, [isOpen]);
+
+  // NOTE(amine): unmout the react app before deleting from the dom
+  if (!isOpen) return null;
+
+  return (
+    <NavigationContext.Provider value={value}>
+      {children}
+    </NavigationContext.Provider>
+  );
 };
