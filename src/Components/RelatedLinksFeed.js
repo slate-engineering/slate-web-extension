89e037855fb9460a6842b774ebed346f9baad6f7
Aminejv <elouartinra@hotmail.com>
2022-05-23 18:51:45 +0100

    feat(RelatedLinksFeed): add navigation using arrows

--- src/Components/RelatedLinksFeed.js
+++ src/Components/RelatedLinksFeed.js
@@ -2,10 +2,10 @@ import * as React from "react";
 import * as Styles from "../Common/styles";
 import * as ListView from "./ListView";
 import * as Navigation from "../Core/navigation/app/jumper";
+import * as RovingTabIndex from "../Components/RovingTabIndex";
 
 import { css } from "@emotion/react";
 import { getFavicon } from "../Common/favicons";
-import { useGetRelatedLinks } from "../Core/history/app";
 
 const STYLES_RELATED_LINKS_POPUP_HEADER = css`
   ${Styles.HORIZONTAL_CONTAINER};
@@ -26,31 +26,34 @@ const STYLES_OBJECT_PREVIEW_LIST_WRAPPER = css`
   animation: object-preview-fade-in 250ms ease;
 `;
 
-export default function RelatedLinksFeed({ url, ...props }) {
-  const relatedLinks = useGetRelatedLinks(url);
-  if (!relatedLinks) return null;
-
+export default function RelatedLinksFeed({ feed, ...props }) {
   return (
-    <ListView.Section {...props}>
-      <ListView.Title
-        css={STYLES_RELATED_LINKS_POPUP_HEADER}
-        count={relatedLinks.length}
-      >
-        Related
-      </ListView.Title>
-      <div css={STYLES_OBJECT_PREVIEW_LIST_WRAPPER}>
-        {relatedLinks
-          .flatMap(({ item: session }) => session.visits)
-          .map((visit, i) => (
-            <ListView.Object
-              key={visit.id + i}
-              title={visit.title}
-              Favicon={getFavicon(visit.rootDomain)}
-              onClick={() => Navigation.openUrls({ urls: [visit.url] })}
-              onMouseEnter={(e) => e.target.focus()}
-            />
-          ))}
-      </div>
-    </ListView.Section>
+    <RovingTabIndex.Provider>
+      <RovingTabIndex.List>
+        <ListView.Root {...props}>
+          <ListView.Section>
+            <ListView.Title
+              css={STYLES_RELATED_LINKS_POPUP_HEADER}
+              count={feed?.length}
+            >
+              Related
+            </ListView.Title>
+            <div css={STYLES_OBJECT_PREVIEW_LIST_WRAPPER}>
+              {feed &&
+                feed.map((visit, i) => (
+                  <RovingTabIndex.Item key={visit.id} index={i}>
+                    <ListView.Object
+                      title={visit.title}
+                      Favicon={getFavicon(visit.rootDomain)}
+                      onClick={() => Navigation.openUrls({ urls: [visit.url] })}
+                      onMouseEnter={(e) => e.target.focus()}
+                    />
+                  </RovingTabIndex.Item>
+                ))}
+            </div>
+          </ListView.Section>
+        </ListView.Root>
+      </RovingTabIndex.List>
+    </RovingTabIndex.Provider>
   );
 }
