8b8639492b73cbafe37b342968048a00109d0cdd
Aminejv <elouartinra@hotmail.com>
2022-05-24 19:42:04 +0100

    feat(Views): show favicons in custom actions

--- src/Components/Views.js
+++ src/Components/Views.js
@@ -2,8 +2,9 @@ import * as React from "react";
 import * as Typography from "../Components/system/Typography";
 import * as Styles from "../Common/styles";
 import * as ListView from "../Components/ListView";
-import * as SVG from "../Common/SVG";
 import * as RovingTabIndex from "../Components/RovingTabIndex";
+import * as SVG from "../Common/SVG";
+import * as Favicons from "../Common/favicons";
 
 import { css } from "@emotion/react";
 import { getFavicon } from "../Common/favicons";
@@ -54,6 +55,7 @@ const STYLES_VIEWS_BUTTON_ACTIVE = (theme) => css`
 
 const STYLES_VIEWS_BUTTON = (theme) => css`
   ${Styles.BUTTON_RESET};
+  ${Styles.HORIZONTAL_CONTAINER_CENTERED};
   border-radius: 12px;
   padding: 5px 12px 7px;
   color: ${theme.semantic.textGray};
@@ -100,17 +102,20 @@ const CUSTOM_VIEWS_ACTIONS = [
   {
     label: "Twitter",
     data: { type: viewsType.relatedLinks, query: "https://twitter.com/" },
+    Favicon: Favicons.getFavicon("twitter.com"),
+  },
+  {
+    label: "Hacker News",
+    data: {
+      type: viewsType.relatedLinks,
+      query: "https://www.hackernews.com",
+    },
+    Favicon: Favicons.getFavicon("hackernews.com"),
   },
   {
     label: "Youtube",
     data: { type: viewsType.relatedLinks, query: "https://www.youtube.com/" },
-  },
-  {
-    label: "Google Searches",
-    data: {
-      type: viewsType.relatedLinks,
-      query: "https://www.google.com/search?",
-    },
+    Favicon: Favicons.getFavicon("youtube.com"),
   },
 ];
 
@@ -148,29 +153,36 @@ function Menu({ css, ...props }) {
 
           <Divider height="none" width="1px" style={{ margin: "0px 4px" }} />
 
-          {CUSTOM_VIEWS_ACTIONS.map((viewAction, i) => (
-            <RovingTabIndex.Item
-              key={viewAction.label}
-              index={VIEWS_ACTIONS.length + i}
-            >
-              <Typography.H5
-                css={[
-                  STYLES_VIEWS_BUTTON,
-                  currentView === viewAction.data.type &&
-                    currentViewQuery === viewAction.data.query &&
-                    STYLES_VIEWS_BUTTON_ACTIVE,
-                ]}
-                style={{ marginLeft: 4 }}
-                as="button"
-                onClick={createOnClickHandler({
-                  type: viewAction.data.type,
-                  query: viewAction.data.query,
-                })}
+          {CUSTOM_VIEWS_ACTIONS.map((viewAction, i) => {
+            const { Favicon } = viewAction;
+            const isApplied =
+              currentView === viewAction.data.type &&
+              currentViewQuery === viewAction.data.query;
+            return (
+              <RovingTabIndex.Item
+                key={viewAction.label}
+                index={VIEWS_ACTIONS.length + i}
               >
-                {viewAction.label}
-              </Typography.H5>
-            </RovingTabIndex.Item>
-          ))}
+                <Typography.H5
+                  css={[
+                    STYLES_VIEWS_BUTTON,
+                    isApplied && STYLES_VIEWS_BUTTON_ACTIVE,
+                  ]}
+                  style={{ marginLeft: 4 }}
+                  as="button"
+                  onClick={createOnClickHandler({
+                    type: viewAction.data.type,
+                    query: viewAction.data.query,
+                  })}
+                >
+                  <Favicon
+                    style={{ marginRight: 4, opacity: isApplied ? 1 : 0.5 }}
+                  />
+                  {viewAction.label}
+                </Typography.H5>
+              </RovingTabIndex.Item>
+            );
+          })}
 
           <RovingTabIndex.Item
             index={VIEWS_ACTIONS.length + CUSTOM_VIEWS_ACTIONS.length}
