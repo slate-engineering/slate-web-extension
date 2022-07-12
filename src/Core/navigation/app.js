c8c2f5ab8ba1842c2cf6e3abc10cef9a3bb8201f
Aminejv <elouartinra@hotmail.com>
2022-07-12 13:13:19 +0100

    chore(Core/Navigation): remove dead code

--- src/Core/navigation/app.js
+++
@@ -1,8 +0,0 @@
-import { messages } from "./";
-
-export const openUrls = ({ urls, query }) =>
-  window.postMessage({ type: messages.openURLsRequest, urls, query }, "*");
-
-export const closeExtensionJumper = () => {
-  window.postMessage({ type: messages.closeExtensionJumperRequest }, "*");
-};
