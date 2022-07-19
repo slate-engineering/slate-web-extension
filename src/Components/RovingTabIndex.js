17c74b16fe778e386f907c454ba274e4750a3922
Aminejv <elouartinra@hotmail.com>
2022-07-19 13:51:28 +0100

    feat(RovingTabIndex): add support for infinite lists

--- src/Components/RovingTabIndex.js
+++ src/Components/RovingTabIndex.js
@@ -49,128 +49,140 @@ const useManageScrollPosition = ({
 };
 
 const rovingIndexContext = React.createContext({});
-const useRovingIndexContext = () => React.useContext(rovingIndexContext);
+export const useRovingIndexContext = () => React.useContext(rovingIndexContext);
 
-const Provider = React.forwardRef(({ axis = "vertical", children }, ref) => {
-  const focusedElementsRefs = React.useRef({});
-  const initialIndex = 0;
-  const [focusedIndex, setFocusedIndex] = React.useState(initialIndex);
+const Provider = React.forwardRef(
+  ({ isInfiniteList, axis = "vertical", children }, ref) => {
+    const focusedElementsRefs = React.useRef({});
+    const initialIndex = 0;
+    const [focusedIndex, setFocusedIndex] = React.useState(initialIndex);
 
-  const registerItem = ({ index, ref }) => {
-    focusedElementsRefs.current[index] = ref;
-    if (ref.current === document.activeElement) setFocusedIndex(index);
-  };
+    const registerItem = ({ index, ref }) => {
+      focusedElementsRefs.current[index] = ref;
+      if (ref.current === document.activeElement) setFocusedIndex(index);
+    };
 
-  const cleanupItem = (index) => {
-    delete focusedElementsRefs.current[index];
-  };
+    const cleanupItem = (index) => {
+      delete focusedElementsRefs.current[index];
+    };
 
-  const listRef = React.useRef();
-  const isFocusWithinBeforeCleanup = React.useRef(false);
-  const registerList = (ref) => {
-    listRef.current = ref.current;
-    //NOTE(amine): if a child element was focused before unmounting, focus the first child when mounting
-    if (isFocusWithinBeforeCleanup.current) {
-      focusElement(focusedIndex);
-      isFocusWithinBeforeCleanup.current = false;
-    }
-  };
-  const cleanupList = () => {
-    setFocusedIndex(0);
-    const listElement = listRef.current;
-    if (!listElement) return;
-    const root = listRef.current.getRootNode() || document;
-    if (listElement.contains(root.activeElement)) {
-      isFocusWithinBeforeCleanup.current = true;
-    }
-  };
+    const listRef = React.useRef();
+    const isFocusWithinBeforeCleanup = React.useRef(false);
+    const registerList = (ref) => {
+      listRef.current = ref.current;
+      //NOTE(amine): if a child element was focused before unmounting, focus the first child when mounting
+      if (isFocusWithinBeforeCleanup.current) {
+        focusElement(focusedIndex);
+        isFocusWithinBeforeCleanup.current = false;
+      }
+    };
+    const cleanupList = () => {
+      setFocusedIndex(0);
+      const listElement = listRef.current;
+      if (!listElement) return;
+      const root = listRef.current.getRootNode() || document;
+      if (listElement.contains(root.activeElement)) {
+        isFocusWithinBeforeCleanup.current = true;
+      }
+    };
 
-  const syncFocusedIndexState = ({ index, isFocused }) => {
-    if (!isFocused) return;
-    const calculatedMaxIndex = Math.max(
-      ...Object.keys(focusedElementsRefs.current)
+    const syncFocusedIndexState = ({ index, isFocused }) => {
+      if (!isFocused) return;
+      const calculatedMaxIndex = Math.max(
+        ...Object.keys(focusedElementsRefs.current)
+      );
+      const maxIndex = Number.isFinite(calculatedMaxIndex)
+        ? calculatedMaxIndex
+        : index;
+      const minIndex = initialIndex;
+      if (index >= maxIndex) {
+        setIndexTo(maxIndex);
+      } else if (index <= minIndex) {
+        setIndexTo(minIndex);
+      } else {
+        setIndexTo(index);
+      }
+    };
+
+    const focusElement = (index) => {
+      const focusedElementRef = focusedElementsRefs.current[index];
+      if (focusedElementRef) {
+        focusedElementRef.current.focus({ preventScroll: true });
+      }
+    };
+
+    const setIndexToNextElement = () => {
+      const nextIndex = focusedIndex + 1;
+      const elementsExists = focusedElementsRefs.current[nextIndex];
+      if (!elementsExists && isInfiniteList) {
+        return;
+      }
+      const nextFocusedIndex = elementsExists ? nextIndex : initialIndex;
+      setFocusedIndex(nextFocusedIndex);
+      focusElement(nextFocusedIndex);
+    };
+
+    const setIndexPreviousElement = () => {
+      const prevIndex = focusedIndex - 1;
+      let prevFocusedIndex = null;
+      if (prevIndex >= initialIndex) {
+        prevFocusedIndex = prevIndex;
+      } else {
+        prevFocusedIndex = Math.max(
+          ...Object.keys(focusedElementsRefs.current)
+        );
+      }
+      setFocusedIndex(prevFocusedIndex);
+      focusElement(prevFocusedIndex);
+    };
+
+    const setIndexTo = (index) => {
+      if (!focusedElementsRefs.current[index]) return;
+      setFocusedIndex(index);
+      focusElement(index);
+    };
+
+    useManageScrollPosition({
+      listRef,
+      focusedElementsRefs,
+      focusedIndex,
+      axis,
+    });
+
+    React.useImperativeHandle(
+      ref,
+      () => ({
+        focusSelectedElement: () => focusElement(focusedIndex),
+      }),
+      [focusedIndex]
     );
-    const maxIndex = Number.isFinite(calculatedMaxIndex)
-      ? calculatedMaxIndex
-      : index;
-    const minIndex = initialIndex;
-    if (index >= maxIndex) {
-      setIndexTo(maxIndex);
-    } else if (index <= minIndex) {
-      setIndexTo(minIndex);
-    } else {
-      setIndexTo(index);
-    }
-  };
 
-  const focusElement = (index) => {
-    const focusedElementRef = focusedElementsRefs.current[index];
-    if (focusedElementRef) {
-      focusedElementRef.current.focus({ preventScroll: true });
-    }
-  };
+    const contextValue = React.useMemo(
+      () => [
+        { focusedIndex, axis },
+        {
+          registerItem,
+          cleanupItem,
 
-  const setIndexToNextElement = () => {
-    const nextIndex = focusedIndex + 1;
-    const elementsExists = focusedElementsRefs.current[nextIndex];
-    const nextFocusedIndex = elementsExists ? nextIndex : initialIndex;
-    setFocusedIndex(nextFocusedIndex);
-    focusElement(nextFocusedIndex);
-  };
+          registerList,
+          cleanupList,
 
-  const setIndexPreviousElement = () => {
-    const prevIndex = focusedIndex - 1;
-    let prevFocusedIndex = null;
-    if (prevIndex >= initialIndex) {
-      prevFocusedIndex = prevIndex;
-    } else {
-      prevFocusedIndex = Math.max(...Object.keys(focusedElementsRefs.current));
-    }
-    setFocusedIndex(prevFocusedIndex);
-    focusElement(prevFocusedIndex);
-  };
+          syncFocusedIndexState,
+          setIndexToNextElement,
+          setIndexPreviousElement,
+          setIndexTo,
+        },
+      ],
+      [focusedIndex]
+    );
 
-  const setIndexTo = (index) => {
-    if (!focusedElementsRefs.current[index]) return;
-    setFocusedIndex(index);
-    focusElement(index);
-  };
-
-  useManageScrollPosition({ listRef, focusedElementsRefs, focusedIndex, axis });
-
-  React.useImperativeHandle(
-    ref,
-    () => ({
-      focusSelectedElement: () => focusElement(focusedIndex),
-    }),
-    [focusedIndex]
-  );
-
-  const contextValue = React.useMemo(
-    () => [
-      { focusedIndex, axis },
-      {
-        registerItem,
-        cleanupItem,
-
-        registerList,
-        cleanupList,
-
-        syncFocusedIndexState,
-        setIndexToNextElement,
-        setIndexPreviousElement,
-        setIndexTo,
-      },
-    ],
-    [focusedIndex]
-  );
-
-  return (
-    <rovingIndexContext.Provider value={contextValue}>
-      {children}
-    </rovingIndexContext.Provider>
-  );
-});
+    return (
+      <rovingIndexContext.Provider value={contextValue}>
+        {children}
+      </rovingIndexContext.Provider>
+    );
+  }
+);
 
 /* -------------------------------------------------------------------------------------------------
  * RovingTabIndex List
