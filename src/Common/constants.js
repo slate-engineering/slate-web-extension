4bff0b302505f3721ef9c3fbfff52ae9b77bd9f5
Aminejv <elouartinra@hotmail.com>
2022-07-28 13:59:07 +0100

    feat(Constants): add jumperFeedItem and jumperFeedWrapper to sizes

--- src/Common/constants.js
+++ src/Common/constants.js
@@ -17,6 +17,8 @@ export const sizes = {
   tablet: 960,
   desktop: 1024,
   desktopM: 1300,
+  jumperFeedItem: 40,
+  jumperFeedWrapper: 373,
   topOffset: 0, //NOTE(martina): Pushes UI down. 16 when there is a persistent announcement banner, 0 otherwise
 };
 
