fad325664fbb38bc70332db64a566e9c2c0973c3
Aminejv <elouartinra@hotmail.com>
2022-09-05 19:53:38 +0100

    fix(views): fix search for the files view

--- src/Core/views/background.js
+++ src/Core/views/background.js
@@ -145,7 +145,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       };
 
       const handleViewsSearch = async () => {
-        if (view.type !== viewsType.saved && view.type !== viewsType.file) {
+        if (view.type !== viewsType.saved && view.type !== viewsType.files) {
           const viewsSearchResult = await Views.search({
             view,
             query,
